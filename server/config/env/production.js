2bf0983ef7b42ebbce5c6dcec749f55f38335264
pauloortins <paulo.ortins@gmail.com>
2014-04-22 18:12:49 -0300

    Added production

---
+++ server/config/env/production.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+    mongodb://<user>:<password>@oceanic.mongohq.com:10083/codeanalyticsdb	
+    db: 'mongodb://paulo.ortins@gmail.com:artimanha@oceanic.mongohq.com:10083/codeanalyticsdb',
+    app: {
+        name: 'MEAN - A Modern Stack - Production'
+    },
+    facebook: {
+        clientID: 'APP_ID',
+        clientSecret: 'APP_SECRET',
+        callbackURL: 'http://localhost:3000/auth/facebook/callback'
+    },
+    twitter: {
+        clientID: 'CONSUMER_KEY',
+        clientSecret: 'CONSUMER_SECRET',
+        callbackURL: 'http://localhost:3000/auth/twitter/callback'
+    },
+    github: {
+        clientID: 'APP_ID',
+        clientSecret: 'APP_SECRET',
+        callbackURL: 'http://localhost:3000/auth/github/callback'
+    },
+    google: {
+        clientID: 'APP_ID',
+        clientSecret: 'APP_SECRET',
+        callbackURL: 'http://localhost:3000/auth/google/callback'
+    },
+    linkedin: {
+        clientID: 'API_KEY',
+        clientSecret: 'SECRET_KEY',
+        callbackURL: 'http://localhost:3000/auth/linkedin/callback'
+    }
+};
