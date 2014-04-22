be23dafd8a62b3dc00452e3ef4aea1a5061b4934
pauloortins <paulo.ortins@gmail.com>
2014-04-22 18:14:47 -0300

    Fix file

--- server/config/env/production.js
+++ server/config/env/production.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = {
-    mongodb://<user>:<password>@oceanic.mongohq.com:10083/codeanalyticsdb	
     db: 'mongodb://paulo.ortins@gmail.com:artimanha@oceanic.mongohq.com:10083/codeanalyticsdb',
     app: {
         name: 'MEAN - A Modern Stack - Production'
